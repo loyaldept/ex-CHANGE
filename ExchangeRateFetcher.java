8471592f231f362a96707fc94b49a7acdef11352
Zuhayr Abdullazhanov <115369065+loyaldept@users.noreply.github.com>
2024-05-02 15:52:35 -0400

    Create ExchangeRateFetcher.java

---
+++ ExchangeRateFetcher.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class ExchangeRateFetcher {
+    private static final String API_KEY = "YOUR_API_KEY";
+
+    public static double getExchangeRate(String fromCurrency, String toCurrency) throws IOException {
+        String url = "https://v6.exchangerate-api.com/v6/" + API_KEY + "/pair/" + fromCurrency + "/" + toCurrency;
+
+        HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+        connection.setRequestMethod("GET");
+
+        int responseCode = connection.getResponseCode();
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String response = reader.readLine();
+            reader.close();
+
+            // Parse the JSON response and extract the exchange rate
+            int startIndex = response.indexOf("\"rate\":") + 7;
+            int endIndex = response.indexOf("}", startIndex);
+            String rateString = response.substring(startIndex, endIndex);
+            return Double.parseDouble(rateString);
+        } else {
+            throw new IOException("Error: " + responseCode);
+        }
+    }
+}
